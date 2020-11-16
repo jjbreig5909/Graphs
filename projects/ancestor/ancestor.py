a51f0098815d1fc7943c2488ed1a9a0bf1c9ea76
Jeff Breig <jjbreig@gmail.com>
2020-11-15 23:54:02 -0500

    last commit of night

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -22,6 +22,12 @@ class Graph:
         """
         self.vertices[v1].add(v2)
 
+    def get_neighbors(self, vertex_id):
+        """
+        Get all neighbors (edges) of a vertex.
+        """
+        return self.vertices[vertex_id]
+
 def earliest_ancestor(ancestors, starting_node):
 
     family_tree =  Graph()
@@ -31,4 +37,11 @@ def earliest_ancestor(ancestors, starting_node):
            family_tree.add_vertex(ancestor)
            
         family_tree.add_edge(pair[0], pair[1])
+
+    stack = [starting_node]
+    depth = {}
     
+    while len(stack) > 0:
+        current_node = stack.pop()
+        neighbors = family_tree.get_neighbors(current_node)
+        
